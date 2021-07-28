4a4aa8f0ebc77c2f1be88c9ecb26bc7d507b3a97
PenguinL <diana1997525@gmail.com>
2021-07-29 00:48:36 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,9 @@
+#in venv
+#pip install -r req.txt
+pip freeze > req.txt
 #git clone -o 0_write_to_file https://github.com/Kovalsky95/0_write_to_file.git
 #git config --global credential.helper cache
-git config --global credential.helper "cache --timeout=3600"
+git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
 #git config --unset credential.helper
 git pull --all
