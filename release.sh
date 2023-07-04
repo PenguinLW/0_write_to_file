2d288b9a4ab014b859c2353a1f9eb478facaa4ed
PenguinLW <diana1997525@gmail.com>
2023-07-04 18:37:58 +0800

    PenguinL

--- release.sh
+++ release.sh
@@ -1,11 +1,14 @@
 #in venv
 #pip install -r req.txt
 #pip freeze > req.txt
+
+#git config --global --add safe.directory /media/penguinl/PenguinL/PycharmProjects/0_write_to_file
 #git clone -o 0_write_to_file https://github.com/PenguinLW/0_write_to_file.git
 #git config --global credential.helper cache
 git config --global credential.helper "cache --timeout=25200"
 #git config credential.helper store
 #git config --unset credential.helper
+
 git pull --all
 git add .
 git commit -m PenguinL
